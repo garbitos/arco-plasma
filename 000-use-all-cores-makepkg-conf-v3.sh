56b850fcd8440f8801be5b33c6eef487b03b460a
garbitos <305984+garbitos@users.noreply.github.com>
2019-09-23 07:41:29 -0400

    Higher thread counts and updated messages for clarity.
    Intel Xeon socket 2011 thread count support.

--- 000-use-all-cores-makepkg-conf-v3.sh
+++ 000-use-all-cores-makepkg-conf-v3.sh
@@ -17,51 +17,84 @@ set -e
 
 numberofcores=$(grep -c ^processor /proc/cpuinfo)
 
-
 case $numberofcores in
 
+    32)
+        echo "You have " $numberofcores" CPU threads available."
+        echo "Changing the makeflags for "$numberofcores" threads."
+        sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j33"/g' /etc/makepkg.conf
+        echo "Changing the compression settings for "$numberofcores" threads."
+        sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 32 -z -)/g' /etc/makepkg.conf
+        ;;
+    24)
+        echo "You have " $numberofcores" CPU threads available."
+        echo "Changing the makeflags for "$numberofcores" threads."
+        sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j25"/g' /etc/makepkg.conf
+        echo "Changing the compression settings for "$numberofcores" threads."
+        sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 24 -z -)/g' /etc/makepkg.conf
+        ;;
+    20)
+        echo "You have " $numberofcores" CPU threads available."
+        echo "Changing the makeflags for "$numberofcores" threads."
+        sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j21"/g' /etc/makepkg.conf
+        echo "Changing the compression settings for "$numberofcores" threads."
+        sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 20 -z -)/g' /etc/makepkg.conf
+        ;;
     16)
-        echo "You have " $numberofcores" cores."
-        echo "Changing the makeflags for "$numberofcores" cores."
+        echo "You have "$numberofcores" CPU threads available."
+        echo "Changing the makeflags for "$numberofcores" threads."
         sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j17"/g' /etc/makepkg.conf
-        echo "Changing the compression settings for "$numberofcores" cores."
+        echo "Changing the compression settings for "$numberofcores" threads."
         sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 16 -z -)/g' /etc/makepkg.conf
         ;;
+    12)
+        echo "You have " $numberofcores" CPU threads available."
+        echo "Changing the makeflags for "$numberofcores" threads."
+        sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j13"/g' /etc/makepkg.conf
+        echo "Changing the compression settings for "$numberofcores" threads."
+        sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 12 -z -)/g' /etc/makepkg.conf
+        ;;
+    10)
+        echo "You have " $numberofcores" CPU threads available."
+        echo "Changing the makeflags for "$numberofcores" threads."
+        sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j11"/g' /etc/makepkg.conf
+        echo "Changing the compression settings for "$numberofcores" threads."
+        sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 10 -z -)/g' /etc/makepkg.conf
+        ;;
     8)
-        echo "You have " $numberofcores" cores."
-        echo "Changing the makeflags for "$numberofcores" cores."
+        echo "You have "$numberofcores" CPU threads available."
+        echo "Changing the makeflags for "$numberofcores" threads."
         sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j9"/g' /etc/makepkg.conf
-        echo "Changing the compression settings for "$numberofcores" cores."
+        echo "Changing the compression settings for "$numberofcores" threads."
         sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 8 -z -)/g' /etc/makepkg.conf
         ;;
     6)
-        echo "You have " $numberofcores" cores."
-        echo "Changing the makeflags for "$numberofcores" cores."
+        echo "You have "$numberofcores" CPU threads available."
+        echo "Changing the makeflags for "$numberofcores" threads."
         sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j7"/g' /etc/makepkg.conf
-        echo "Changing the compression settings for "$numberofcores" cores."
+        echo "Changing the compression settings for "$numberofcores" threads."
         sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 6 -z -)/g' /etc/makepkg.conf
         ;;
     4)
-        echo "You have " $numberofcores" cores."
-        echo "Changing the makeflags for "$numberofcores" cores."
+        echo "You have "$numberofcores" CPU threads available."
+        echo "Changing the makeflags for "$numberofcores" threads."
         sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j5"/g' /etc/makepkg.conf
-        echo "Changing the compression settings for "$numberofcores" cores."
+        echo "Changing the compression settings for "$numberofcores" threads."
         sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 4 -z -)/g' /etc/makepkg.conf
         ;;
     2)
-        echo "You have " $numberofcores" cores."
-        echo "Changing the makeflags for "$numberofcores" cores."
+        echo "You have "$numberofcores" CPU threads available."
+        echo "Changing the makeflags for "$numberofcores" threads."
         sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j3"/g' /etc/makepkg.conf
-        echo "Changing the compression settings for "$numberofcores" cores."
+        echo "Changing the compression settings for "$numberofcores" threads."
         sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 2 -z -)/g' /etc/makepkg.conf
         ;;
     *)
-        echo "We do not know how many cores you have."
-        echo "Do it manually."
+        echo "Update the scipt manually for "$numberofcores" thread support."
         ;;
 
 esac
 
-echo "################################################################"
-echo "###  All cores will be used during building and compression ####"
-echo "################################################################"
+echo "##################################################################################"
+echo "### "$numberofcores" CPU threads will be used during building and compression ####"
+echo "##################################################################################"
